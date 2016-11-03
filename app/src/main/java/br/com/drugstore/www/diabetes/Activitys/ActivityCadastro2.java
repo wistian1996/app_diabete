0ea9185c418cc1a0c1bb8415422ebd0662a94488
Alexandre Andrade <alexandre.aprendiz@gmail.com>
2016-11-03 07:56:41 -0200

    salvar a seleção da tipo, término do cadastro do usuário e redirecionar para tela principal

--- app/src/main/java/br/com/drugstore/www/diabetes/Activitys/ActivityCadastro2.java
+++ app/src/main/java/br/com/drugstore/www/diabetes/Activitys/ActivityCadastro2.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import br.com.drugstore.www.diabetes.DAO.DAOUsuario;
 import br.com.drugstore.www.diabetes.R;
 
 public class ActivityCadastro2 extends AppCompatActivity {
 
 
     private Button buttonAvancar;
-    private RadioButton radioButton1;
-    private RadioButton radioButton2;
-    private RadioButton radioButtonOutro;
+    private RadioGroup radioGroup;
+    // iteração com o banco de dados da tabela usuario
+    private DAOUsuario daoUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +26,42 @@ public class ActivityCadastro2 extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro2);
 
         buttonAvancar = (Button) findViewById(R.id.buttonAvancar);
-        radioButton1 = (RadioButton) findViewById(R.id.radioButton1);
-        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
-        radioButtonOutro = (RadioButton) findViewById(R.id.radioButtonOutro);
-
+        radioGroup = (RadioGroup) findViewById(R.id.radioGrupo);
         //definindo acao para o botao avancar
-     buttonAvancar.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent intent = new Intent(ActivityCadastro2.this, MainActivity.class);
-            startActivity(intent);
-             finish();
-         }
-     });
+        buttonAvancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // seleciona o radiobutton do grupo
+                int selecionado = radioGroup.getCheckedRadioButtonId();
+                // checa qual radiobutton foi selecionado e seta o tipo de diabete do usuario a ser cadastrado
+                switch (selecionado){
+                    case R.id.rbTipo1:
+                        ActivityCadastro.usuario.setTipoDiabete("Tipo 1");
+                    case R.id.rbTipo2:
+                        ActivityCadastro.usuario.setTipoDiabete("Tipo 2");
+                    case R.id.rbOutro:
+                        ActivityCadastro.usuario.setTipoDiabete("Outro");
+                    default:
+                        break;
+                }
+                // faz a inserção do usuário no banco
+                daoUsuario = new DAOUsuario(getApplicationContext());
+                long i = daoUsuario.inserirUsuario(ActivityCadastro.usuario);
+                if(i > 0){
+                    Toast.makeText(getApplicationContext(),"Usuário cadastrado com sucesso!",Toast.LENGTH_SHORT).show();
+                    telaPrincipal();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Erro ao cadastrar",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
+    // chama a tela principal
+    public void telaPrincipal(){
+        Intent intent = new Intent(ActivityCadastro2.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
