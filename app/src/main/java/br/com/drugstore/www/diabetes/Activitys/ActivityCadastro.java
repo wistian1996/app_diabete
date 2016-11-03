01837b9bb94626e34bfd5ea21c853257b9e5b117
ptmais <alexandre.aprendiz@gmail.com>
2016-11-03 07:52:47 -0200

    inserção do DAOUsuário para cadastrar ou verificar se existe usuário, foco para quando ocorrer erro nos campos e método para ir para tela principal caso tenha usuário cadastrado

--- app/src/main/java/br/com/drugstore/www/diabetes/Activitys/ActivityCadastro.java
+++ app/src/main/java/br/com/drugstore/www/diabetes/Activitys/ActivityCadastro.java
@@ -10,24 +10,37 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import br.com.drugstore.www.diabetes.DAO.DAOUsuario;
 import br.com.drugstore.www.diabetes.Domain.Usuario;
 import br.com.drugstore.www.diabetes.R;
 
 public class ActivityCadastro extends AppCompatActivity {
+
     private Spinner spinnerSexo;
     private Button buttonAvancar;
     private EditText editTextNome;
     private EditText editTextIdade;
     // serve para listar as opçoes no spinner e no list view
     private ArrayAdapter<String> opcoesSpinner;
-
+    // usuario a ser cadastrado
+    public static Usuario usuario = new Usuario();
+    // iteração com o banco de dados da tabela usuario
+    private DAOUsuario daoUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_cadastro);
+
+        daoUsuario = new DAOUsuario(getApplicationContext());
+        ArrayList<Usuario> list = daoUsuario.getTodosUsuarios();
+        // caso ja possua usuário cadastrado, será redirecionado para página principal
+        if(list.size() > 0){
+            telaPrincipal();
+        }
+
         editTextNome = (EditText) findViewById(R.id.editTextNome);
         editTextIdade = (EditText) findViewById(R.id.editTextIdade);
         spinnerSexo = (Spinner) findViewById(R.id.spinnerSexo);
@@ -48,16 +61,12 @@ public class ActivityCadastro extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (verificarCampos()) {
-
                     Intent intent = new Intent(ActivityCadastro.this, ActivityCadastro2.class);
                     startActivity(intent);
                     finish();
                 }
-
             }
         });
-
-
     }
 
     private boolean verificarCampos() {
@@ -67,39 +76,53 @@ public class ActivityCadastro extends AppCompatActivity {
         String nome;
 
         try {
-            idade = Integer.parseInt(editTextIdade.getText().toString());
-            if (idade <= 0 || idade > 150) {
-                editTextIdade.setError("Idade inválida!");
+            nome = editTextNome.getText().toString();
+            if (nome.trim().equals("")) {
+                // exibindo mensagem na tela
+                editTextNome.setError("Preencha seu nome!");
+                // setando o foco para o erro
+                editTextNome.requestFocus();
                 return false;
             }
         } catch (Exception e) {
-            editTextIdade.setError("Idade inválida!");
+            editTextNome.setError("Nome inválido");
+            // setando o foco para o erro
+            editTextNome.requestFocus();
             return false;
         }
 
         try {
-            nome = editTextNome.getText().toString();
-
-
-            if (nome.trim().equals("")) {
-                // exibindo mensagem na tela
-                editTextNome.setError("Preencha seu nome!");
+            idade = Integer.parseInt(editTextIdade.getText().toString());
+            if (idade <= 0 || idade > 150) {
+                editTextIdade.setError("Idade inválida!");
+                // setando o foco para o erro
+                editTextIdade.requestFocus();
                 return false;
             }
-
         } catch (Exception e) {
-            editTextNome.setError("Nome inválido");
+            editTextIdade.setError("Preencha sua idade!");
+            // setando o foco para o erro
+            editTextIdade.requestFocus();
             return false;
         }
 
         sexo = spinnerSexo.getSelectedItem().toString();
-
         if (sexo.equals("Informe seu sexo")) {
             Toast toast = Toast.makeText(getApplicationContext(), "Selecione seu sexo", Toast.LENGTH_SHORT);
             toast.show();
             return false;
         }
-
+        // caso não ocorra erro, será setado os dados do usuário
+        usuario.setNome(nome);
+        usuario.setSexo(sexo);
+        usuario.setId(idade);
         return true;
+    }
+
+    // chama a tela principal
+    public void telaPrincipal(){
+        Intent intent = new Intent(ActivityCadastro.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
