51fddf638eadeb1bec4a2215820231fc4937bde5
Alexandre Andrade <alexandre.aprendiz@gmail.com>
2016-11-03 08:19:38 -0200

    talvez não seja necessário (estudar o caso de uso)

--- app/src/main/java/br/com/drugstore/www/diabetes/Domain/HorariosAlarme.java
+++ app/src/main/java/br/com/drugstore/www/diabetes/Domain/HorariosAlarme.java
@@ -1,39 +1,46 @@
 package br.com.drugstore.www.diabetes.Domain;
 
+import java.util.Date;
+
 /**
  * Created by wisti on 31/10/2016.
  */
 public class HorariosAlarme {
     private int id;
     private int idAlarme;
-    private int hour;
-    private int minute;
+    private Date horario;
 
-    HorariosAlarme(int id , int idAlarme, int hour , int minute){
-        this.idAlarme = idAlarme;
-        this.id = id;
-        this.hour = hour;
-        this.minute = minute;
-
+    public HorariosAlarme() {
     }
 
-    HorariosAlarme(int idAlarme, int hour , int minute){
-        this.idAlarme = idAlarme;
+    public HorariosAlarme(int id, int idAlarme, Date horario) {
         this.id = id;
-        this.hour = hour;
-        this.minute = minute;
-
+        this.idAlarme = idAlarme;
+        this.horario = horario;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getHour() {
-        return hour;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getMinute() {
-        return minute;
+    public int getIdAlarme() {
+        return idAlarme;
     }
+
+    public void setIdAlarme(int idAlarme) {
+        this.idAlarme = idAlarme;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+
 }
